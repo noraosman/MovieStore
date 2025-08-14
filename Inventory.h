da11cf4c4a960467c4a67d8598e076fa4d18a984
Kyle <kylewilliams342@gmail.com>
2025-08-13 17:39:35 -0700

    Updated movie.h

--- Inventory.h
+++ Inventory.h
@@ -1,1 +1,27 @@
-//// ---------------------------------------------------------------------------// Assignment 4: Movie Store// Group Members: Kyle Williams(2550507) and Nora Osmanova(2550505)// Date: 08/12/2025//// Description:// Can be treated as a separate transaction. Shows stock// ---------------------------------------------------------------------------//#ifndef INVENTORY_H#define INVENTORY_H#endif //INVENTORY_H
+//
+// ---------------------------------------------------------------------------
+// Assignment 4: Movie Store
+// Group Members: Kyle Williams(2550507) and Nora Osmanova(2550505)
+// Date: 08/12/2025
+//
+// Description:
+// Can be treated as a separate transaction. Shows stock
+// ---------------------------------------------------------------------------
+//
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include "Movie.h"
+
+class Inventory {
+public:
+    void showInventory(Movie);
+    void showInventoryHelper(Movie);
+    bool addMovie(Movie);
+private:
+    Movie comedyRoot;
+    Movie classicRoot;
+    Movie dramaRoot;
+};
+
+#endif INVENTORY_H
